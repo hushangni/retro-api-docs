8af29c499c98e6ac107d0a18e13b6e64b95ed20f
Shangni <shangnihu@gmail.com>
2019-07-24 15:06:11 -0400

    Updates

---
+++ precache-manifest.a58378a15585cd6645be86c47d3ce23e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ebef75fb9f954d428ef562e88fbd01",
+    "url": "/retro-api-docs/index.html"
+  },
+  {
+    "revision": "32a908c06a8d84588407",
+    "url": "/retro-api-docs/static/css/main.860daf41.chunk.css"
+  },
+  {
+    "revision": "9b90f2af11bd5815bb14",
+    "url": "/retro-api-docs/static/js/2.3c3a15fb.chunk.js"
+  },
+  {
+    "revision": "32a908c06a8d84588407",
+    "url": "/retro-api-docs/static/js/main.2f0966dd.chunk.js"
+  },
+  {
+    "revision": "20b3fd749248195d4e73",
+    "url": "/retro-api-docs/static/js/runtime~main.45dda416.js"
+  },
+  {
+    "revision": "d6f674bbe8fb6eba849ca1edfd6e8509",
+    "url": "/retro-api-docs/static/media/background.d6f674bb.jpg"
+  },
+  {
+    "revision": "94d60ad042fb25af30b81152f98e1b65",
+    "url": "/retro-api-docs/static/media/logo.94d60ad0.png"
+  }
+]);
